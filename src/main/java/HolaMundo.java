80dd8a6aca60a68c5908a92a816499f174e1db1e
Fulloa08 <f.ulloa08@ufromail.cl>
2024-09-02 16:44:00 -0400

    se sube codigo para actividad de github colaborativa

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -1,7 +1,9 @@
+package Proyecto.src.main.java;
 
-public class HolaMundo {
+public class  HolaMundo{
     public static void main(String[] args) {
         System.out.println("Hola Mundo!!!!");
+        System.out.println("Felipe Ulloa");
     }
 
 }
